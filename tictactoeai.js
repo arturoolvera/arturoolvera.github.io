f5c975346d0514124f0184717726201837c05416
Arturo Olvera <arturoolvera99@gmail.com>
2021-10-20 12:53:14 -0700

    mobile bug fix v2

--- tictactoeai.js
+++ tictactoeai.js
@@ -37,6 +37,11 @@ var b = function ( sketch ) {
         return false;
     }
 
+    sketch.touchStarted = function() {
+        sketch.mouseClicked();
+        return false;
+    }
+
 
     sketch.checkWin = function() {
         winstates = [[0,1,2], [3,4,5], [6,7,8], [0,4,8], [6,4,2], [0,3,6], [1,4,7],
